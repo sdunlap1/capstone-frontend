777c8a6b368bc19eb432e6bc34cbdbe9697077b0
sdunlap1 <sdunlap1@mac.com>
2024-10-23 16:37:32 -0700

    Changed to BASE_URL

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,13 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://10.0.4.23:3001";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://10.0.4.23:3001',
+  baseURL: BASE_URL,
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');  // Assuming you're storing JWT in localStorage
+  const token = localStorage.getItem("token"); // Assuming you're storing JWT in localStorage
   if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
+    config.headers["Authorization"] = `Bearer ${token}`;
   }
   return config;
 });
