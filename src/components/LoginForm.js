fcabc3e89f81b56a248c67c435ca1e2c3f2671e7
sdunlap1 <sdunlap1@mac.com>
2024-11-18 21:55:49 -0800

    Changed alert messages to show on the modal instead of a pop-up

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,28 +5,31 @@ import { login } from "../services/authService";
 const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
+  const [invalidUnPw, setInvalidUnPw] = useState(false);
+  const [emptyFields, setEmptyFields] = useState(false);
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     const trimmedUsername = username.trim();
     const trimmedPassword = password.trim();
+    
+    setInvalidUnPw(false);
+    setEmptyFields(false);
 
     if (!trimmedUsername || !trimmedPassword) {
-      alert("Please fill out all fields.");
+      setEmptyFields(true);
       return;
     }
 
     try {
       const user = await login(trimmedUsername, trimmedPassword); // Use trimmed values here
       localStorage.setItem("token", user.token);
-
       // Redirect to dashboard by default after login
       navigate("/dashboard");
     } catch (err) {
       console.error("Login failed:", err);
-      setError("Invalid username or password. Please try again.");
+      setInvalidUnPw(true);
     }
   };
 
@@ -34,6 +37,12 @@ const LoginForm = () => {
     <div className="form-container">
       <div className="form-box">
         <h1 className="heading">Please Login</h1>
+        {emptyFields && (
+         <div className="error-text">Please fill out all fields.</div>
+        )}
+        {invalidUnPw && (
+          <div className="error-text">Invalid username or password.</div>
+        )}
     <form onSubmit={handleLogin}>
       <input
         type="text"
@@ -49,7 +58,6 @@ const LoginForm = () => {
         placeholder="Password"
         autoComplete="current-password"
       />
-      {error && <p style={{ color: "red" }}>{error}</p>}
       <button className="auth-button" type="submit">Login</button>
     </form>
     </div>
